9e206a77c5a0592cbf697f919d43429b91b2ed1f
Dromich <hrushch.rr57@gmail.com>
2019-10-12 22:18:19 +0300

    add row prijekt folder

---
+++ tradefilms/app/js/common.js
@@ -0,0 +1,199 @@
+$(function() {
+
+	$.getJSON("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5", function(data){
+		var usd_data = "";
+		$.each(data, function(key, value){
+			usd_data += value.sale;
+		});
+		var show = Number(usd_data[0] + usd_data[1] + usd_data[2] + usd_data[3] + usd_data[4]);
+		var show1 = (show + 0.29).toPrecision(3);
+		$("#usd-rate").append(show1);
+	});
+
+
+	$("form.callback").submit(function() { 
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "/mail.php", 
+			data: th.serialize()
+		}).done(function() {
+			$(th).find('.success').addClass('active').css('display', 'flex').hide().fadeIn();
+			setTimeout(function() {
+				$(th).find('.success').removeClass('active').fadeOut();
+				th.trigger("reset");
+			}, 3000);
+		});
+		return false;
+	});
+	
+
+
+
+
+	function isVisible(elem) {
+
+		var coords = elem.getBoundingClientRect();
+
+		var windowHeight = document.documentElement.clientHeight;
+
+		var topVisible = coords.top > 0 && coords.top < windowHeight;
+		var bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
+
+		return topVisible || bottomVisible;
+	}
+
+	function showVisible() {
+		var imgs = document.getElementsByTagName('img');
+		for (var i = 0; i < imgs.length; i++) {
+
+			var img = imgs[i];
+
+			var realsrc = img.getAttribute('realsrc');
+			if (!realsrc) continue;
+
+			if (isVisible(img)) {
+				img.src = realsrc;
+				img.setAttribute('realsrc', '');
+			}
+		}
+	}
+	window.onscroll = showVisible;
+	showVisible();
+
+
+
+
+
+	$(window).scroll(function(){
+		$('.navbar-dark').toggleClass('navbar-active', $(this).scrollTop() > 400);
+	});
+
+	$('.clients').owlCarousel({
+		loop: true,
+		smartSpeed: 700,
+		nav: false,
+		autoplay: true,
+		autoplayTimeout: 6000,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			576: {
+				items: 2
+			},
+			768:{
+				items: 2
+			},
+			992: {
+				items: 3
+			}, 
+			1200: {
+				items: 4
+			}
+		}
+	});
+
+
+
+	$('.partners').owlCarousel({
+		loop: true,
+		smartSpeed: 700,
+		nav: false,
+		autoplay: true,
+		autoplayTimeout: 6000,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			576: {
+				items: 1
+			},
+			768:{
+				items: 2
+			},
+			992: {
+				items: 2
+			}, 
+			1200: {
+				items: 2
+			}
+		}
+	});
+
+
+
+
+
+
+	$('.certificates').owlCarousel({
+		loop: true,
+		smartSpeed: 700,
+		nav: true,
+		autoplay: true,
+		autoplayTimeout: 12000,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			576: {
+				items: 1
+			},
+			768:{
+				items: 1
+			},
+			992: {
+				items: 1
+			}, 
+			1200: {
+				items: 1
+			}
+		}
+	});
+
+	// Custom JS
+
+	$( '.on-top' ).on( 'click', function(e){
+		var href = $(this).attr( 'href' );
+		$( 'html, body' ).animate({
+			scrollTop: $( href ).offset().top
+		}, '300' );
+		e.preventDefault();
+
+	});
+
+	
+	$(window).scroll(function(){
+		if ($(this).scrollTop()*4 > $(this).height()){
+			$(".on-top").addClass("on-top_active");
+		}
+		else $(".on-top").removeClass("on-top_active");
+	})
+
+});
+
+
+
+$( '.ultravision-link').on( 'click', function(e){
+	var href = $(this).attr( 'href' );
+	$( 'html, body' ).animate({
+		scrollTop: $( href ).offset().top
+	}, '300' );
+	e.preventDefault();
+
+});
+
+$(document).mouseup(function (e) {
+	var container = $(".sidenav");
+	if (container.has(e.target).length === 0){
+		container.removeClass("sidenav-close");
+		$(".menu-btn").click(function(){
+			$(".sidenav").toggleClass("sidenav-close");
+		});
+
+	}
+});
+
---
+++ tradefilms/gulpfile.js
@@ -0,0 +1,94 @@
+var syntax        = 'sass'; // Syntax: sass or scss;
+
+var gulp          = require('gulp'),
+		gutil         = require('gulp-util' ),
+		sass          = require('gulp-sass'),
+		browserSync   = require('browser-sync'),
+		concat        = require('gulp-concat'),
+		uglify        = require('gulp-uglify'),
+		cleancss      = require('gulp-clean-css'),
+		rename        = require('gulp-rename'),
+		autoprefixer  = require('gulp-autoprefixer'),
+		notify        = require("gulp-notify"),
+		rsync         = require('gulp-rsync'),
+		ftp           = require('vinyl-ftp');
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// open: false,
+		// online: false, // Work Offline Without Internet Connection
+		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
+	})
+});
+
+gulp.task('styles', function() {
+	return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
+	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+	.pipe(rename({ suffix: '.min', prefix : '' }))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream())
+});
+
+gulp.task('js', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/owl/owl.carousel.min.js',
+		'app/libs/bootstrap/bootstrap.min.js',
+		'app/js/common.js', // Always at the end
+		])
+	.pipe(concat('scripts.min.js'))
+	.pipe(uglify()) // Mifify js (opt.)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('rsync', function() {
+	return gulp.src('app/**')
+	.pipe(rsync({
+		root: 'app/',
+		hostname: 'nazar105@tradefilms.com.ua',
+		destination: 'yousite/public_html/',
+		// include: ['*.htaccess'], // Includes files to deploy
+		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
+		recursive: true,
+		archive: true,
+		silent: false,
+		compress: true
+	}))
+});
+
+gulp.task('watch', ['styles', 'js', 'browser-sync'], function() {
+	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', ['styles']);
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
+	gulp.watch('app/*.html', browserSync.reload)
+});
+
+
+gulp.task('deploy', function() {
+
+	var conn = ftp.create({
+		host:      'tradefilms.com.ua',
+		user:      'nazar105',
+		password:  'tradefilms',
+		parallel:  10,
+		log: gutil.log
+	});
+
+	var globs = [
+	'dist/**',
+	];
+	return gulp.src(globs, {buffer: false})
+	.pipe(conn.dest('/public_html'));
+
+});
+
+
+
+
+gulp.task('default', ['watch']);
